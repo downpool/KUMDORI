60898ec31dca35fc064f32755ec8f27e20bf3e68
donghyeon choi <downpool.choi@gmail.com>
2024-05-25 18:52:05 +0900

    init

---
+++ KUMDORI/View/Charactor.swift
@@ -0,0 +1,18 @@
+//
+//  Chractor.swift
+//  KUMDORI
+//
+//  Created by donghyeon choi on 5/25/24.
+//
+
+import SwiftUI
+
+struct Charactor: View {
+    var body: some View {
+        Image(URL(string: <#T##String#>))
+    }
+}
+
+#Preview {
+    Charactor()
+}
