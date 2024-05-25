03ca8500cefa47e48b497ff21efdf47d6d2b3060
donghyeon choi <downpool.choi@gmail.com>
2024-05-25 18:42:37 +0900

    Initial Commit

---
+++ KUMDORI/KUMDORIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KUMDORIApp.swift
+//  KUMDORI
+//
+//  Created by donghyeon choi on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct KUMDORIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
